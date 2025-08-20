9a495a2295b3a28ca408f372451843632d4608eb
Aryan Khanijo <akhanijo12345@gmail.com>
2025-08-20 17:01:46 +0530

    Connect invoices app API to main project URLs.Connect invoices app API to main project URLs.Update urls.py

--- backend/invoice_project/urls.py
+++ backend/invoice_project/urls.py
@@ -5,6 +5,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # Connect invoices app API to main project URLs
     path('api/', include('invoices.urls')),
 ]
 
